fc8adc3477b4f920168b27e2fef9619c84a89fb1
SXWOOD <71347500+SXWOOD@users.noreply.github.com>
2020-10-21 19:12:57 -0500

    Update scatter.py
    Testing button placement within GUI window, buttons ARE NOT connected yet

--- src/scatter.py
+++ src/scatter.py
@@ -1,6 +1,6 @@
 import logging
-from PySide2 import QtWidgets, QtCore
-from PySide2.QtWidgets import QApplication, QHBoxLayout, QSlider, QLabel
+from PySide2 import QtWidgets, QtCore, QtGui
+from PySide2.QtWidgets import QHBoxLayout, QSlider, QLabel
 from PySide2.QtCore import Qt
 from shiboken2 import wrapInstance
 import maya.OpenMayaUI as omui
@@ -32,25 +32,24 @@ class ScatterToolUI(QtWidgets.QDialog):
         self.create_connections()
 
     def create_ui(self):
-        self.title_lbl = QtWidgets.QLabel("Scatter Tool")
-        self.title_lbl.setStyleSheet("font: bold 20px")
-        self.folder_lay = self._create_folder_ui()
-        self.folder_lay_2 = self._create_folder_ui_2()
+        self.scatter_with = self._create_scatter_with()
+        self.scatter_to = self._create_scatter_to()
+        self.slider = self.create_slider()
         self.filename_lay = self._create_filename_ui()
         self.button_lay = self._create_button_ui()
         self.main_lay = QtWidgets.QVBoxLayout()
-        self.main_lay.addWidget(self.title_lbl)
-        self.main_lay.addLayout(self.folder_lay)
-        self.main_lay.addLayout(self.folder_lay_2)
+        self.main_lay.addLayout(self.scatter_with)
+        self.main_lay.addLayout(self.scatter_to)
+        self.main_lay.addLayout(self.slider)
         self.main_lay.addLayout(self.filename_lay)
         self.main_lay.addStretch()
         self.main_lay.addLayout(self.button_lay)
         self.setLayout(self.main_lay)
-        self.create_slider()
+        #self.create_slider()
 
     def create_slider(self):
         """Slider to change density from 0 to 100%"""
-        hbox= QHBoxLayout()
+        hbox = QHBoxLayout()
 
         self.slider = QSlider()
         self.slider.setOrientation(Qt.Horizontal)
@@ -60,9 +59,12 @@ class ScatterToolUI(QtWidgets.QDialog):
         self.slider.valueChanged.connect(self.change_density)
 
         self.label = QLabel("0")
+        self.label.setFont(QtGui.QFont("Sanserif", 15))
 
         hbox.addWidget(self.slider)
         hbox.addWidget(self.label)
+
+        #self.setLayout(hbox)
 
     def change_density(self):
         size = self.slider.value()
@@ -83,7 +85,7 @@ class ScatterToolUI(QtWidgets.QDialog):
         self.scenefile.folder_path = self.folder_le.text()
         self.scenefile.descriptor = self.descriptor_le.text()
         self.scenefile.task = self.task_le.text()
-        self.scenefile.ver = self.ver_sbx.value()
+        self.scenefile.ver = self.ver_le.value()
         self.scenefile.ext = self.ext_lbl.text()
 
     @QtCore.Slot()
@@ -101,40 +103,33 @@ class ScatterToolUI(QtWidgets.QDialog):
         layout.addWidget(self.scatter_btn)
         return layout
 
-
     def _create_filename_ui(self):
         layout = self._create_filename_headers()
         self.descriptor_le = QtWidgets.QLineEdit(self.scenefile.descriptor)
-        self.descriptor_le.setMinimumWidth(100)
+        self.descriptor_le.setFixedWidth(50)
         self.task_le = QtWidgets.QLineEdit(self.scenefile.task)
         self.task_le.setFixedWidth(50)
-        self.ver_sbx = QtWidgets.QSpinBox()
-        self.ver_sbx.setButtonSymbols(QtWidgets.QAbstractSpinBox.PlusMinus)
-        self.ver_sbx.setFixedWidth(50)
-        self.ver_sbx.setValue(self.scenefile.ver)
-        self.ext_lbl = QtWidgets.QLabel(".ma")
+        self.ver_le = QtWidgets.QLineEdit(self.scenefile.descriptor)
+        self.ver_le.setFixedWidth(50)
+        self.ver_le = QtWidgets.QLineEdit(self.scenefile.descriptor)
+        self.ext_lbl = QtWidgets.QLabel("Random Scale Minimum")
         layout.addWidget(self.descriptor_le, 1, 0)
-        layout.addWidget(QtWidgets.QLabel("_"), 1, 1)
         layout.addWidget(self.task_le, 1, 2)
-        layout.addWidget(QtWidgets.QLabel("_v"), 1, 3)
-        layout.addWidget(self.ver_sbx, 1, 4)
+        layout.addWidget(self.ver_le, 1, 4)
         layout.addWidget(self.ext_lbl, 1, 5)
         return layout
 
     def _create_filename_headers(self):
-        self.descriptor_header_lbl = QtWidgets.QLabel("Descriptor")
-        self.descriptor_header_lbl.setStyleSheet("font: bold")
-        self.task_header_lbl = QtWidgets.QLabel("Task")
-        self.task_header_lbl.setStyleSheet("font: bold")
-        self.ver_header_lbl = QtWidgets.QLabel("Version")
-        self.ver_header_lbl.setStyleSheet("font: bold")
+        self.scale_x_lbl = QtWidgets.QLabel("X")
+        self.scale_y_lbl = QtWidgets.QLabel("Y")
+        self.scale_z_lbl = QtWidgets.QLabel("Z")
         layout = QtWidgets.QGridLayout()
-        layout.addWidget(self.descriptor_header_lbl, 0, 0)
-        layout.addWidget(self.task_header_lbl, 0, 2)
-        layout.addWidget(self.ver_header_lbl, 0, 4)
+        layout.addWidget(self.scale_x_lbl, 0, 0)
+        layout.addWidget(self.scale_y_lbl, 0, 2)
+        layout.addWidget(self.scale_z_lbl, 0, 4)
         return layout
 
-    def _create_folder_ui(self):
+    def _create_scatter_with(self):
         default_folder = Path(cmds.workspace(rootDirectory=True, query=True))
         default_folder = default_folder / "scenes"
         self.folder_le = QtWidgets.QLineEdit(default_folder)
@@ -144,7 +139,7 @@ class ScatterToolUI(QtWidgets.QDialog):
         layout.addWidget(self.folder_browse_btn)
         return layout
 
-    def _create_folder_ui_2(self):
+    def _create_scatter_to(self):
         default_folder = Path(cmds.workspace(rootDirectory=True, query=True))
         default_folder = default_folder / "scenes"
         self.folder_le = QtWidgets.QLineEdit(default_folder)
@@ -211,19 +206,4 @@ class SceneFile(object):
             self.folder_path.makedirs_p()
             return pmc.system.saveAs(self.path)
 
-    def next_avail_ver(self):
-        """Return the next available version number in the folder."""
-        pattern = "{descriptor}_{task}_v*{ext}".format(
-            descriptor=self.descriptor, task=self.task, ext=self.ext)
-        matching_scenefiles = []
-        for file_ in self.folder_path.files():
-            if file_.name.fnmatch(pattern):
-                matching_scenefiles.append(file_)
-        if not matching_scenefiles:
-            return 1
-        matching_scenefiles.sort(reverse=True)
-        latest_scenefile = matching_scenefiles[0]
-        latest_scenefile = latest_scenefile.name.stripext()
-        latest_ver_num = int(latest_scenefile.split("_v")[-1])
-        return latest_ver_num + 1
 
